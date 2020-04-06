13a0292301b687538b51cbb56040354c796eb98e
Henri de la Hoz <cyberdelahoz95@gmail.com>
2020-04-05 20:50:04 -0500

    Implementing player service

--- angular-first-app/src/app/app.module.ts
+++ angular-first-app/src/app/app.module.ts
@@ -7,16 +7,17 @@ import { environment } from 'src/environments/environment';
 
 import { AngularFireModule } from 'angularfire2';
 import { AngularFireDatabaseModule } from 'angularfire2/database';
+import { PlayerService } from './services/player.service';
 
 @NgModule({
-  declarations: [AppComponent],
-  imports: [
-    BrowserModule,
-    AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFireDatabaseModule,
-    AppRoutingModule,
-  ],
-  providers: [],
-  bootstrap: [AppComponent],
+    declarations: [AppComponent],
+    imports: [
+        BrowserModule,
+        AngularFireModule.initializeApp(environment.firebaseConfig),
+        AngularFireDatabaseModule,
+        AppRoutingModule,
+    ],
+    providers: [PlayerService],
+    bootstrap: [AppComponent],
 })
 export class AppModule {}
